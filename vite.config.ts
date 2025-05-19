c9a8955f4d9ef613613c9987f1b7a8aefb9d7c4e
Jonathan Bessette <jonathan.bessette@bell.ca>
2025-05-19 15:36:25 -0400

    Attempt to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,14 @@
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/plexview_glass_website/",
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer()
-          ),
-        ]
-      : []),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "src"),
-      "@assets": path.resolve(import.meta.dirname, "src/assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "."),
-  build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
-    emptyOutDir: true,
-  },
-  publicDir: "public",
+  base: "/plexview_glass_website/",
 });
