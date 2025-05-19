8023f42330f0a4a7467780a13f2051e88741d666
Jonathan Bessette <74223159+JoBess7@users.noreply.github.com>
2025-05-18 21:21:43 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/plexview_glass_website/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
